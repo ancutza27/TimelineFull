e2099bab710a35ede09aae6d3cd6d3d9980e938e
Anabela <anabela.rossi@gmail.com>
2013-11-22 15:22:17 -0300

    Atributos Mensaje.

--- src/timelineme/model/Mensaje.java
+++ src/timelineme/model/Mensaje.java
@@ -1,5 +1,58 @@
 package timelineme.model;
 
+import java.util.List;
+
 public class Mensaje {
+		private Integer idMensaje;
+		private String contenido;
+		private String fecha;
+		private Integer idAgente;
+		private Integer idEmpresa;
+
+		public Integer getId() {
+			return this.idMensaje;
+		}
+		
+		public void setId(Integer idMensaje) {
+			this.idMensaje = idMensaje;
+		}
+		
+		
+		public String getContenido() {
+			return this.contenido;
+		}
+		
+		public void setContenido(String contenido) {
+			this.contenido = contenido;
+		}
+		
+		
+		
+		public String getFecha() {
+			return this.fecha;
+		}
+		
+		public void setFecha(String string) {
+			this.fecha = string;
+		}
+		
+		
+		public Integer getIdAgente() {
+			return this.idAgente;
+		}
+		
+		public void setIdAgente(Integer id) {
+			this.idAgente = id;
+		}
+		
+		
+		
+		public Integer getIdEmpresa() {
+			return this.idEmpresa;
+		}
+		
+		public void setIdEmpresa(Integer idE) {
+			this.idEmpresa = idE;
+		}
 
 }
